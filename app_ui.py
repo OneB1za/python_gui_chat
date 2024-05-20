d62267b3b5dd5a5d676ed770a7d0c9d692ac82de
Your Name <kiril.kulikow@yandex.ru>
2024-05-20 19:31:49 +0400

    v0.6

--- app_ui.py
+++ app_ui.py
@@ -1,8 +1,8 @@
 import re
+import threading
+import websocket
 from tkinter import *
 from tkinter import messagebox
-import threading
-
 from client import Client
 from server import Server
 
@@ -17,23 +17,83 @@ class ChatApp:
         self.client_instance = None
         self.server_thread = None
 
+        self.frame_system_buttons = Frame(self.master, pady=10)
+        self.login_frame = Frame(self.master)
+        self.register_frame = Frame(self.master)
+        self.chat_frame = Frame(self.master)  # NEW
+
         self.create_widgets()
 
+
+    '''Виджеты'''
+
     def create_widgets(self):
-        frame_system_buttons = Frame(self.master, pady=10)
-        frame_system_buttons.pack(fill=X)
+
+        self.frame_system_buttons.pack(fill=X)
 
         # Кнопка создать чат
-        btn_create_chat = Button(frame_system_buttons, text="Создать чат", command=self.start_server_thread)
+        btn_create_chat = Button(self.frame_system_buttons, text="Создать чат", command=self.start_server_thread)
         btn_create_chat.pack(side=LEFT, padx=10)
 
-        # Кнопка выйти из чата
-        btn_close_connection = Button(self.master, text='Выйти', command=self.stop_client)
-        btn_close_connection.pack(side=LEFT, padx=10)
-
         # Кнопка подключиться к чату
-        btn_connect_chat = Button(frame_system_buttons, text='Подключиться к чату', command=self.connect_to_chat)
+        btn_connect_chat = Button(self.frame_system_buttons, text='Подключиться к чату', command=self.connect_to_chat)
         btn_connect_chat.pack(side=LEFT, padx=10)
+
+        # Кнопка авторизации
+        btn_login = Button(self.frame_system_buttons, text='Авторизоваться', command=self.show_log_frame)
+        btn_login.pack(side=LEFT, padx=10)
+
+        # авторизация
+
+        Label(self.login_frame, text='Имя пользователя:').pack()
+        self.login_username = Entry(self.login_frame)
+        self.login_username.pack()
+
+        Label(self.login_frame, text='Пароль:').pack()
+        self.login_password = Entry(self.login_frame, show='*')
+        self.login_password.pack()
+
+        Button(self.login_frame, text="Войти", command=self.login).pack()
+        Button(self.login_frame, text="Зарегистрироваться", command=self.show_reg_frame).pack()
+        Button(self.login_frame, text='Назад', command=self.show_general_frame).pack()
+
+        # регистрация
+
+        Label(self.register_frame, text="Имя пользователя:").pack()
+        self.register_username = Entry(self.register_frame)
+        self.register_username.pack()
+
+        Label(self.register_frame, text="Пароль:").pack()
+        self.register_password = Entry(self.register_frame, show="*")
+        self.register_password.pack()
+
+        Label(self.register_frame, text="Эл.Почта").pack()
+        self.register_email = Entry(self.register_frame)
+        self.register_email.pack()
+
+        Button(self.register_frame, text='Зарегистрироваться', command=self.register).pack()
+        Button(self.register_frame, text="Авторизоваться", command=self.show_log_frame).pack()
+        Button(self.register_frame, text='Назад', command=self.show_general_frame).pack()
+
+        # Окно чата
+        self.text_box = Text(self.chat_frame, height=20, width=50)
+        self.text_box.pack(side=LEFT, fill=BOTH, expand=True)
+
+        scrollbar = Scrollbar(self.chat_frame, command=self.text_box.yview)
+        scrollbar.pack(side=RIGHT, fill=Y)
+
+        self.text_box.config(yscrollcommand=scrollbar.set, state=DISABLED)
+
+        self.entry = Entry(self.chat_frame, width=50)
+        self.entry.pack(padx=10, pady=5)
+
+        send_button = Button(self.chat_frame, text="Отправить", command=self.send_message_to_server)
+        send_button.pack(pady=5)
+
+        back_button = Button(self.chat_frame, text='Выйти', command=self.stop_client)
+        back_button.pack(pady=5)
+
+    '''Сообщение о запуске сервера'''
 
     def custom_message_box_server_start(self):
         message_window = Toplevel(self.master)
@@ -45,11 +105,11 @@ class ChatApp:
                                                f'Ваш ip для подключения {ip_message}')
         msg_label.pack(padx=20, pady=20)
 
-        def copy_text():
+        def _copy_text():
             message_window.clipboard_clear()
             message_window.clipboard_append(ip_message)
 
-        copy_btn = Button(message_window, text='Скопировать IP', command=copy_text)
+        copy_btn = Button(message_window, text='Скопировать IP', command=_copy_text)
         copy_btn.pack(pady=10)
 
         close_button = Button(message_window, text="Закрыть", command=message_window.destroy)
@@ -58,42 +118,36 @@ class ChatApp:
         message_window.grab_set()
         message_window.wait_window()
 
-    ''' '''
+    def check_ip_wrapper(self):
+        ip = self.ent_ip.get()
+        # self.check_ip(ip)
+        threading.Thread(target=self.check_ip, args=(ip,)).start()
 
-    def create_chat_window(self):
+    '''Проверка на корректность данных для подключения'''
 
-        self.chat_window = Toplevel(self.master)
-        self.chat_window.title('Диалог')
-        self.chat_window.geometry('640x720')
-        self.message_window.destroy()
-
-        text_frame = Frame(self.chat_window)
-        text_frame.pack(padx=10, pady=10)
-
-        self.text_box = Text(text_frame, height=20, width=50)
-        self.text_box.pack(side=LEFT, fill=BOTH, expand=True)
-
-        scrollbar = Scrollbar(text_frame, command=self.text_box.yview)
-        scrollbar.pack(side=RIGHT, fill=Y)
-
-        self.text_box.config(yscrollcommand=scrollbar.set, state=DISABLED)
-
-        self.entry = Entry(self.chat_window, width=50)
-        self.entry.pack(padx=10, pady=5)
-
-        send_button = Button(self.chat_window, text="Отправить", command=self.send_message_to_server)
-        send_button.pack(pady=5)
-
-        back_button = Button(self.chat_window, text='Выйти', command=self.stop_client)
-        back_button.pack(pady=5)
-
-    def check_ip(ip):  # TODO добавить в использование перед btn_complete
-        ip_regex = r'^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$'
-        # соответствует ли строка IP
+    def check_ip(self, ip):
+        ip_regex = r'^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$'  # ip
         if re.match(ip_regex, ip):
-            return True
+            try:
+                messagebox.showinfo(title='Подключение', message='Подключаемся... Ожидайте')
+                self.btn_complete['state'] = 'disable'
+                ws = websocket.create_connection(f"ws://{ip}:8765")
+                ws.close()
+                self.master.after(0, lambda: self.on_success_check_ip(ip))
+            except Exception:
+                self.btn_complete['state'] = 'normal'
+                self.master.after(0, lambda: self.on_error(
+                    'Подключение не установлено, т.к. конечный компьютер отверг запрос на подключение'))
         else:
-            return False
+            self.master.after(0, lambda: self.on_error('Введите корректный ip адрес'))
+
+    def on_error(self, message):
+        messagebox.showerror(title='Ошибка', message=message)
+
+    def on_success_check_ip(self, ip):
+        self.start_client(ip)
+        messagebox.showinfo(title='Успех', message=f'Вы подключились к {self.ent_ip.get()}')
+        self.show_chat_frame()
 
     def connect_to_chat(self):
         self.stop_client()  # Останавливаем предыдущий клиент перед созданием нового
@@ -103,17 +157,11 @@ class ChatApp:
         msg_label = Label(self.message_window, text='Введите ip чтобы подключиться к чату')
         msg_label.pack(padx=20, pady=3)
 
-        ent_ip = Entry(self.message_window, width=30)
-        ent_ip.pack()
+        self.ent_ip = Entry(self.message_window, width=30)
+        self.ent_ip.pack()
 
-        btn_complete = Button(self.message_window, text='Подключиться',  # create_chat_window
-                              command=lambda: [self.master.after(0, self.start_client, ent_ip.get()),
-                                               messagebox.showinfo(title='Успех',
-                                                                   message=f'Вы подключились к {ent_ip.get()}'),
-                                               self.create_chat_window()])
-
-        btn_complete.pack(pady=10)
-        btn_complete.pack(pady=10)
+        self.btn_complete = Button(self.message_window, text='Подключиться', command=self.check_ip_wrapper)
+        self.btn_complete.pack(pady=10)
 
         self.message_window.grab_set()
         self.message_window.wait_window()
@@ -156,7 +204,11 @@ class ChatApp:
         if self.client_instance:
             self.client_instance.stop_client_thread()
             self.client_instance = None  # Сбрасываем ссылку на экземпляр клиента
-            self.chat_window.destroy()
+
+            self.hide_all_frames()
+            self.frame_system_buttons.pack(fill=X)
+
+    '''Создания экземпляра клиента при подключении к серверу'''
 
     def start_client(self, ip):
         self.client_instance = Client(ip, app)
@@ -167,6 +219,52 @@ class ChatApp:
         if message.strip():  # Проверяем, что сообщение не пустое
             self.client_instance.send_message(message)
             self.entry.delete(0, END)
+
+    def login(self):
+        username = self.login_username.get()
+        password = self.login_password.get()
+
+        response = None
+
+    def register(self):
+        username = self.register_username.get()
+        password = self.register_password.get()
+        mail = self.register_email.get()
+
+        respose = None
+
+    '''Сокрытие всех фреймов'''
+
+    def hide_all_frames(self):
+        self.frame_system_buttons.pack_forget()
+        self.register_frame.pack_forget()
+        self.login_frame.pack_forget()
+        self.chat_frame.pack_forget()
+
+    '''Окно авторизации'''
+
+    def show_log_frame(self):
+        self.hide_all_frames()
+        self.login_frame.pack(fill='both', expand=True)
+
+    '''Окно регистрации'''
+
+    def show_reg_frame(self):
+        self.hide_all_frames()
+        self.register_frame.pack(fill='both', expand=True)
+
+    '''Стартовое окно'''
+
+    def show_general_frame(self):
+        self.hide_all_frames()
+        self.frame_system_buttons.pack(fill=X)
+
+    '''Создания окна с текстовым полем чата'''
+
+    def show_chat_frame(self):
+        self.message_window.destroy()
+        self.hide_all_frames()
+        self.chat_frame.pack(fill=BOTH, expand=True)
 
 
 if __name__ == "__main__":
